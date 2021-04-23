14f907557f666a81962930827d3933db5ce9f05f
siwolsmu89 <siwol_smuire89@naver.com>
2021-04-23 16:02:51 +0900

    Update Controller -> RestController (ajax request error 404 fixed by this change) 2021/04/23

--- todo-back-spring/src/main/java/ms/kwon/simple/todolist/controller/TodoController.java
+++ todo-back-spring/src/main/java/ms/kwon/simple/todolist/controller/TodoController.java
@@ -3,16 +3,15 @@ package ms.kwon.simple.todolist.controller;
 import com.google.gson.Gson;
 import ms.kwon.simple.todolist.service.TodoService;
 import ms.kwon.simple.todolist.vo.Todo;
-import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/todo")
 public class TodoController {
 
@@ -39,14 +38,13 @@ public class TodoController {
         System.out.println("@@@ TodoController :: getTodolist.do start");
 
         List<Todo> todolist = todoService.getTodolist();
-        System.out.println(todolist.size());
         for (Todo todo : todolist) {
             System.out.println("text: " + todo.getText());
         }
         Gson gson = new Gson();
         String result = gson.toJson(todolist);
 
-        System.out.println(result);
+        System.out.println("@@@ TodoController :: result = " + result);
         System.out.println("@@@ TodoController :: getTodolist.do end");
         return result;
     }
