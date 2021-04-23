26e526e17882297700411053e9714905c6f80c67
siwolsmu89 <siwol_smuire89@naver.com>
2021-04-23 16:01:52 +0900

    Delete unused import for "@MapperScan" 2021/04/23

--- todo-back-spring/src/main/java/ms/kwon/simple/todolist/TodoBackSpringApplication.java
+++ todo-back-spring/src/main/java/ms/kwon/simple/todolist/TodoBackSpringApplication.java
@@ -1,6 +1,5 @@
 package ms.kwon.simple.todolist;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
